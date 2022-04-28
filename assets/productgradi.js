ef96757da0bd20e823b52dd03d6fd02ea5fb8c3f
luiscruzgradiweb <luis.cruz@gradiweb.com>
2022-04-28 12:46:04 -0500

    .

---
+++ assets/productgradi.js
@@ -0,0 +1,77 @@
+
+
+const updateProuct=(variant)=>{
+    products={
+        id:variant.id,
+        quantity:variant.quantity,
+    }
+
+    return products;
+}
+
+const addToCart= () => {
+
+    const products={
+        id:undefined,
+        quantity:0,
+    };
+
+    updateProuct(products)
+    alert('Hello world');
+       
+    // await fetch(window.Shopify.routes.root + 'cart/add.js', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(formData)
+    //   })
+    //   .then(response => {
+    //     return response.json();
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error:', error);
+    //   })
+    //   .finally(() => {
+    //     window.document.location.href = "https://armando-gradi-store.myshopify.com/cart"
+    //   }); 
+}
+
+
+
+const buttonsAdd=document.getElementsByClassName('button_productgrid')
+    for(button of buttonsAdd){
+        button.addEventListener('click', addToCart)
+    } 
+
+let inputs = document.getElementsByClassName("input_productgrid")
+for(item of inputs) {
+    item.addEventListener("mouseover", function(e) {
+        
+        let image = e.target.getAttribute("image_hover")
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        imageProduct.setAttribute("src", image)
+        
+    })
+
+    item.addEventListener("mouseout", function(e) {
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        let image = imageProduct.getAttribute("defaultImage")
+        imageProduct.setAttribute("src", image)
+        
+    })
+
+    item.addEventListener("click", function(e) {
+        let image = e.target.getAttribute("image_hover")
+        let imageProduct = document.getElementById(e.target.getAttribute("idImageProd"))
+        let variant = e.target.getAttribute("variant")
+        let idProduct = e.target.getAttribute("idProduct")
+        let button=document.getElementById(idProduct)
+        imageProduct.setAttribute("src", image)
+        imageProduct.setAttribute("defaultImage", image)
+        button.setAttribute("currentVariant", variant)
+        
+    })
+
+
+}
