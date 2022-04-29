15bff0939d3582f14468e1f6e2ab97855736b519
luiscruzgradiweb <luis.cruz@gradiweb.com>
2022-04-29 13:51:01 -0500

    .

--- assets/productgradi.js
+++ assets/productgradi.js
@@ -1,3 +1,27 @@
+class Productsgrid{
+    #variant = 1234
+    #quantity = 1
+
+    setProductID(idProduct){
+        this.#variant=idProduct
+        alert('si entre')
+    }
+    setProductQuantity(quantity){
+        this.#quantity=quantity
+    }
+
+    getProduct(){
+        return {
+            id:this.#variant,
+            quantity:this.#quantity
+        }
+    }
+    resetProduct(){
+        this.#variant=undefined
+        this.#quantity=1
+    }
+}
+
 const productgrid= new Productsgrid();
 
 const updateProduct=(variant)=>{
@@ -79,26 +103,3 @@ for(item of inputs) {
 
 }
 
-class Productsgrid{
-    #variant = 1234
-    #quantity = 1
-
-    setProductID(idProduct){
-        this.#variant=idProduct
-        alert('si entre')
-    }
-    setProductQuantity(quantity){
-        this.#quantity=quantity
-    }
-
-    getProduct(){
-        return {
-            id:this.#variant,
-            quantity:this.#quantity
-        }
-    }
-    resetProduct(){
-        this.#variant=undefined
-        this.#quantity=1
-    }
-}
